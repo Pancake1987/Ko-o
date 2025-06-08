6a6b331bf68ef339c581a3a3f45b702e0e2ccfea
Kacper Zagozda <255424@edu.p.lodz.pl>
2025-06-08 21:33:07 +0200

    kolo.cpp

--- kolo.cpp
+++ kolo.cpp
@@ -57,12 +57,3 @@ double Point::pole(const Point& p) const {
     double r = radius(p);
     return r * r * 3.1415926535;
 }
-const Point& Point::distant(const Point& p) const {
-    double d = sqrt( m_x * m_x + m_y * m_y);
-    double dp = sqrt(p.m_x * p.m_x + p.m_y * p.m_y);
-
-    if (d > dp)
-        return *this;
-
-    return p;
-}
